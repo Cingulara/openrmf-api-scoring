bfc6f9c779ceef375f62c6d6122f6f835d9f446e
Dale Bingham <dale.bingham@soteriasoft.com>
2024-04-19 06:37:40 -0400

    updated image and components

--- src/Data/ScoreRepository.cs
+++ src/Data/ScoreRepository.cs
@@ -20,78 +20,38 @@ namespace openrmf_scoring_api.Data {
 
         public async Task<IEnumerable<Score>> GetAllScores()
         {
-            try
-            {
                 return await _context.Scores
                         .Find(_ => true).ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
         }
 
         // query after Id or InternalId (BSonId value)
         public async Task<Score> GetScore(string id)
         {
-            try
-            {
                 ObjectId internalId = GetInternalId(id);
                 return await _context.Scores.Find(Score => Score.InternalId == GetInternalId(id)).FirstOrDefaultAsync();
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
         }
 
         // query after artifactId
         public async Task<Score> GetScorebyArtifact(string artifactId)
         {
-            try
-            {
                 return await _context.Scores.Find(Score => Score.artifactId == GetInternalId(artifactId)).FirstOrDefaultAsync();
-             }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
         }
 
         // get all scores by system name
         public async Task<IEnumerable<Score>> GetScoresbySystem(string systemGroupId)
         {
-            try
-            {
                 var query = _context.Scores.Find(Score => Score.systemGroupId == systemGroupId);
                 return await query.ToListAsync();
-             }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
         }
 
         // query after body text, updated time, and header image size
         //
         public async Task<IEnumerable<Score>> GetScore(string bodyText, DateTime updatedFrom, long headerSizeLimit)
         {
-            try
-            {
                 var query = _context.Scores.Find(Score => Score.title.Contains(bodyText) &&
                                     Score.updatedOn >= updatedFrom);
 
                 return await query.ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
         }
 
         private ObjectId GetInternalId(string id)
--- src/Startup.cs
+++ src/Startup.cs
@@ -13,8 +13,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Prometheus;
-using OpenTracing;
-using OpenTracing.Util;
 
 using openrmf_scoring_api.Models;
 using openrmf_scoring_api.Data;
@@ -42,24 +40,6 @@ namespace openrmf_scoring_api
                     options.ConnectionString = Environment.GetEnvironmentVariable("DBCONNECTION");
                     options.Database = Environment.GetEnvironmentVariable("DB");
                 });
-            }
-            
-            if (Environment.GetEnvironmentVariable("JAEGER_AGENT_HOST") != null && 
-                !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JAEGER_AGENT_HOST"))) {
-            
-                // Use "OpenTracing.Contrib.NetCore" to automatically generate spans for ASP.NET Core
-                services.AddSingleton<ITracer>(serviceProvider =>  
-                {                
-                    var loggerFactory = new LoggerFactory();
-                    // use the environment variables to setup the Jaeger endpoints
-                    var config = Jaeger.Configuration.FromEnv(loggerFactory);
-                    var tracer = config.GetTracer();
-                
-                    GlobalTracer.Register(tracer);  
-                
-                    return tracer;  
-                });
-                services.AddOpenTracing();
             }
             
             // added the database repo
